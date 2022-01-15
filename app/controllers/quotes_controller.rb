3a31915ad830d671646e1229938fee6a411848ea
Anna Winkler <3526523+annawinkler@users.noreply.github.com>
2022-01-15 18:26:01 +0400

    Raise an exception

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,12 @@
 class QuotesController < Rulers::Controller
+  # http://localhost:3001/quotes/a_quote
   def a_quote
-    'There is nothing either good or bad but thinking it makes it so.'.freeze
+    'There is nothing either good or bad but thinking it makes it so.'
+    #    + "\n<pre>#{env}</pre>".freeze
+  end
+
+  # http://localhost:3001/quotes/exception
+  def exception
+    raise "It's a bad exception!"
   end
 end
