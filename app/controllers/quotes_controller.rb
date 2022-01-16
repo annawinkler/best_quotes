6f9847e3fbb8baa9060430501dc5d8c970a6274c
Anna Winkler <3526523+annawinkler@users.noreply.github.com>
2022-01-16 18:39:59 +0400

    update render

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,8 +1,7 @@
 class QuotesController < Rulers::Controller
   # http://localhost:3001/quotes/a_quote
   def a_quote
-    'There is nothing either good or bad but thinking it makes it so.'
-    #    + "\n<pre>#{env}</pre>".freeze
+    render :a_quote, noun: :winking
   end
 
   # http://localhost:3001/quotes/exception
